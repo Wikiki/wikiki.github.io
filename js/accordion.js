ef0008c13f3dfb2ed57bda719f261c759be9bcd5
Wikiki <wikiki@protonmail.com>
2018-01-12 08:00:14 +0100

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -1,23 +1,25 @@
-let accordions = document.querySelectorAll('.accordions');
-if (accordions) {
-  accordions.forEach(accordion => {
-    let items = accordion.querySelectorAll('.accordion');
-    if (items) {
-      items.forEach(item => {
-        item.querySelector('.toggle').addEventListener('click', e => {
-          e.preventDefault();
-          let currentItem = e.target.parentNode.parentNode;
-          if (!currentItem.classList.contains('is-active')) {
-            let activeItem = accordion.querySelector('.accordion.is-active');
-            if (activeItem) {
-              activeItem.classList.remove('is-active');
+ready(() => {
+  let accordions = document.querySelectorAll('.accordions');
+  if (accordions) {
+    accordions.forEach(accordion => {
+      let items = accordion.querySelectorAll('.accordion');
+      if (items) {
+        items.forEach(item => {
+          item.querySelector('.toggle').addEventListener('click', e => {
+            e.preventDefault();
+            let currentItem = e.target.parentNode.parentNode;
+            if (!currentItem.classList.contains('is-active')) {
+              let activeItem = accordion.querySelector('.accordion.is-active');
+              if (activeItem) {
+                activeItem.classList.remove('is-active');
+              }
+              currentItem.classList.add('is-active');
+            } else {
+              currentItem.classList.remove('is-active');
             }
-            currentItem.classList.add('is-active');
-          } else {
-            currentItem.classList.remove('is-active');
-          }
+          });
         });
-      });
-    }
-  });
-}
+      }
+    });
+  }
+});
