6e7ed39d796c534e2ff78a3918fdfd66a4cba208
Wikiki <wikiki@protonmail.com>
2018-01-12 07:59:28 +0100

    Update polyfill.js

--- js/polyfill.js
+++ js/polyfill.js
@@ -93,3 +93,15 @@ if (typeof Object.assign != 'function') {
     configurable: true
   });
 }
+
+
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+
