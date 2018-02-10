8d1c3cd63d13071ef5362eec02ed0ddc7ce1af9a
Wikiki <wikiki@protonmail.com>
2018-02-10 10:32:36 +0100

    Update to last extensions versions

--- js/carousel.js
+++ js/carousel.js
@@ -1,40 +1,75 @@
-const MOUSE_EVENTS = ['click', 'touchstart'];
-
 class Carousel {
-  constructor(element) {
-    this.element = element;
+  constructor(selector) {
+    // Determine click event depending on if we are on Touch device or not
+    this._clickEvent = ('ontouchstart' in window) ? 'touchstart' : 'click';
+
+    this.element = typeof selector === 'string' ? document.querySelector(selector) : selector;
+    // An invalid selector or non-DOM node has been provided.
+    if (!this.element) {
+      throw new Error('An invalid selector or non-DOM node has been provided.');
+    }
 
     this.init();
   }
 
   init() {
     this.items = Array.from(this.element.querySelectorAll('.carousel-item'));
+    this.computedStyle = window.getComputedStyle(this.element);
 
-    MOUSE_EVENTS.forEach((event) => {
-      this.element.querySelector('.carousel-nav-left').addEventListener(event, (e) => {
-        this.move('previous');
-        if (this.autoplayInterval) {
-          clearInterval(this.autoplayInterval);
-          this.autoPlay(this.element.dataset.delay || 5000);
-        }
-      }, false);
-      this.element.querySelector('.carousel-nav-right').addEventListener(event, (e) => {
-        this.move('next');
-        if (this.autoplayInterval) {
-          clearInterval(this.autoplayInterval);
-          this.autoPlay(this.element.dataset.delay || 5000);
-        }
-      }, false);
-    });
+    this.previousControl = this.element.querySelector('.carousel-nav-left');
+    this.nextControl = this.element.querySelector('.carousel-nav-right');
 
-    this.initOrder();
+    this._bindEvents();
+    this._initOrder();
 
-    if (this.element.dataset.autoplay && this.element.dataset.autoplay == 'true') {
-      this.autoPlay(this.element.dataset.delay || 5000);
+    if (this.element.dataset._autoPlay && this.element.dataset._autoPlay == 'true') {
+      this._autoPlay(this.element.dataset.delay || 5000);
     }
   }
 
-  initOrder() {
+  /**
+   * Bind all events
+   * @method _bindEvents
+   * @return {void}
+   */
+  _bindEvents() {
+    if (this.previousControl) {
+      this.previousControl.addEventListener(this._clickEvent, (e) => {
+        e.preventDefault();
+        this._slide('previous');
+        if (this._autoPlayInterval) {
+          clearInterval(this._autoPlayInterval);
+          this._autoPlay(this.element.dataset.delay || 5000);
+        }
+      }, false);
+    }
+    if (this.nextControl) {
+      this.nextControl.addEventListener(this._clickEvent, (e) => {
+        e.preventDefault();
+        this._slide('next');
+        if (this._autoPlayInterval) {
+          clearInterval(this._autoPlayInterval);
+          this._autoPlay(this.element.dataset.delay || 5000);
+        }
+      }, false);
+    }
+
+    this.element.addEventListener('touchstart', (e) => {
+      this._touchStart(e);
+    });
+    this.element.addEventListener('mousedown', (e) => {
+      this._touchStart(e);
+    });
+
+    this.element.addEventListener('touchend', (e) => {
+      this._touchEnd(e);
+    });
+    this.element.addEventListener('mouseup', (e) => {
+      this._touchEnd(e);
+    });
+  }
+
+  _initOrder() {
     const currentActiveItem = this.element.querySelector('.carousel-item.is-active');
     const currentActiveItemPos = this.items.indexOf(currentActiveItem);
     const length = this.items.length;
@@ -44,10 +79,10 @@ class Carousel {
     } else {
       this.items.unshift(this.items.pop());
     }
-    this.setOrder();
+    this._setOrder();
   }
 
-  setOrder() {
+  _setOrder() {
     this.items.forEach((item, index) => {
       if (index !== 1) {
         item.style['z-index'] = '0';
@@ -58,7 +93,44 @@ class Carousel {
     });
   }
 
-  move(direction = 'next') {
+  _touchStart(e) {
+    this._touch = {
+      start: {
+        x: e.clientX,
+        y: e.clientY
+      },
+      end: {
+        x: e.clientX,
+        y: e.clientY
+      }
+    }
+  }
+
+  _touchEnd(e) {
+    this._touch.end = {
+      x: e.clientX,
+      y: e.clientY
+    }
+
+    this._handleGesture();
+  }
+
+  _handleGesture() {
+    const ratio = {
+      horizontal: (this._touch.end.x - this._touch.start.x) / parseInt(this.computedStyle.getPropertyValue('width')),
+      vertical: (this._touch.end.y - this._touch.start.y) / parseInt(this.computedStyle.getPropertyValue('height'))
+    };
+
+    if (ratio.horizontal > ratio.vertical && ratio.horizontal > 0.25) {
+      this._slide('previous');
+    }
+
+    if (ratio.horizontal < ratio.vertical && ratio.horizontal < -0.25) {
+      this._slide('next');
+    }
+  }
+
+  _slide(direction = 'next') {
     if (this.items.length) {
       const currentActiveItem = this.element.querySelector('.carousel-item.is-active');
       let newActiveItem;
@@ -74,7 +146,7 @@ class Carousel {
       } else {
         // Reorder items
         this.items.push(this.items.shift());
-        // remove reverse class
+        // re_slide reverse class
         this.element.classList.remove('is-reversing');
       }
 
@@ -84,7 +156,7 @@ class Carousel {
         newActiveItem = this.items[0];
       }
       newActiveItem.classList.add('is-active');
-      this.setOrder();
+      this._setOrder();
 
       // Disable transition to instant change order
       this.element.classList.toggle('carousel-animated');
@@ -95,9 +167,15 @@ class Carousel {
     }
   }
 
-  autoPlay(delay = 5000) {
-    this.autoplayInterval = setInterval(() => {
-      this.move('next');
+  /**
+   * Initiate autoplay system
+   * @method _autoPlay
+   * @param  {Number}  [delay=5000] Delay between slides in milliseconds
+   * @return {void}
+   */
+  _autoPlay(delay = 5000) {
+    this._autoPlayInterval = setInterval(() => {
+      this._slide('next');
     }, delay);
   }
 }
@@ -108,3 +186,5 @@ document.addEventListener('DOMContentLoaded', function() {
     new Carousel(carousel);
   });
 });
+
+// export default Carousel;
