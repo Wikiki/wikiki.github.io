73ec660a7172e3345569b6dd39be1aeb5b189a67
Gaetan <gaetan@hexadog.com>
2019-10-23 10:48:21 +0200

    reset sources

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var deploy      = require('gulp-gh-pages');
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src("./_site/**/*")
+  return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
