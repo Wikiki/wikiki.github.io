589e128c674d3e038b33e572907703f61eb13b89
Gaetan <gaetan@hexadog.com>
2019-10-23 11:47:20 +0200

    last update

---
+++ gulp-tasks/buildJekyll.js
@@ -0,0 +1,13 @@
+var exec = require('child_process').exec,
+	/** Utilities */
+	gutil = require('gulp-util');
+
+module.exports = function buildJekyll(callback, env) {
+	var cmd = 'jekyll build --config ';
+	cmd += (env === 'prod' ? '_config.build.yml' : '_config.yml');
+
+	return exec(cmd, function (error, stdout, stderror) {
+		gutil.log(stdout); // Log the output to the console
+		return callback(error !== null ? 'ERROR: Jekyll process exited with code: ' + error.code : null);
+	});
+};
