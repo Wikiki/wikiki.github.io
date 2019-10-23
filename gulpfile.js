86ba0d9ad82def6b6906e4d49230fe13b98d59ff
Gaetan <gaetan@hexadog.com>
2019-10-23 11:30:18 +0200

    fix(gulp): Wrong deployment folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var deploy      = require('gulp-gh-pages');
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
+  return gulp.src("./_site/**/*")
     .pipe(deploy())
 });
